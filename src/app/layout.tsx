7ca9f070262e0028ed522e1b2bbbf81e9b86a683
Konstantin Khaletskiy <41511242+konstkhal@users.noreply.github.com>
2024-02-24 21:49:21 +0200

    fix(landing): Fix wrong text

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
-import { Poppins } from "next/font/google";
+// import { Poppins } from "next/font/google";
 import "./globals.css";
 import { ReactNode } from "react";
 import React from "react";
 
 // const poppins = Poppins({ weight: "600", subsets: ["latin"] });
-
-export const metadata = {
-  title: "NextJS Course App",
-  description: "Your first NextJS app!",
-};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
