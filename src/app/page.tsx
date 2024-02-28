94078751a7db2510e87527adf226c2ebec0684b6
Konstantin Khaletsky <41511242+konstkhal@users.noreply.github.com>
2024-02-29 00:18:06 +0200

    feat: :sparkles: Add signin page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,33 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <main>
-      <div className=" flex flex-col items-center justify-center h-screen">
-        <div className="content-area flex flex-col items-center p-4 max-w-xs w-full font-poppins">
-          <Image
-            src="/landing__logo.png"
-            alt="First"
-            width="184"
-            height="244"
-            className="mb-4 max-w-full h-auto"
-          />
-          <h1 className="landing-text-width text-header mb-4 text-4xl font-semibold">
-            T-CAre
-          </h1>
-          <p className="landing-text-width text-paragraph mb-4">
-            Self-management App for Cancer
-          </p>
-          <Image
-            src="/landing__ribbon.png"
-            alt="Second"
-            width="351"
-            height="416"
-            className="max-w-full h-auto"
-          />
-        </div>
-      </div>
-    </main>
+    <>
+      <header className="bg-gray-200 py-4" />
+      <main className="content-area flex flex-col items-center p-4 max-w-xs w-full">
+        <Image
+          src="/landing__logo.png"
+          alt="First"
+          width="184"
+          height="244"
+          className="mb-4 max-w-full h-auto"
+        />
+        {/*    <h1 className="landing-text-width text-header mb-4 text-4xl font-semibold">
+          T-CAre
+        </h1> */}
+        <p className="landing-text-width text-paragraph mb-4 font-poppins text-16 font-semibold leading-24 tracking-normal text-center text-blue-900">
+          <Link href="/signin/"> Self-management App for Cancer </Link>
+        </p>
+        <Image
+          src="/landing__ribbon.png"
+          alt="Second"
+          width="351"
+          height="416"
+          className="max-w-full h-auto"
+        />
+      </main>
+      <footer className="bg-gray-200 py-4" />
+    </>
   );
 }
