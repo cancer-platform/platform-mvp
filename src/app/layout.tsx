7641987380968132ee9a64f504f7800b403517fa
Konstantin Khaletsky <41511242+konstkhal@users.noreply.github.com>
2024-02-28 20:29:51 +0200

    chore: :wastebasket: Remove bash.exe.stackdump

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,5 @@
-// import { Poppins } from "next/font/google";
-import "./globals.css";
 import { ReactNode } from "react";
-import React from "react";
-
-// const poppins = Poppins({ weight: "600", subsets: ["latin"] });
+import "./globals.css";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
